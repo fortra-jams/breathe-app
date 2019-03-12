53b9e75ea2452db71022ac9ddf964c3d6ac410ef
pepe moe <muhammad24lokman@gmail.com>
2019-03-12 16:00:11 +0800

    added card in home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'addtask', loadChildren: './addtask/addtask.module#AddtaskPageModule' },
 
 ];
 
