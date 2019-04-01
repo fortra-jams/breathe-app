f6d358b1f36a11bb3a32a577e521d1e3ea570cf7
unknown <anandsanjey@gmail.com>
2019-04-01 14:22:42 +0800

    addtask

--- src/app/addtask/addtask.module.ts
+++ src/app/addtask/addtask.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { AddtaskPage } from './addtask.page';
 
 const routes: Routes = [
@@ -18,9 +16,12 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
   declarations: [AddtaskPage]
+
 })
 export class AddtaskPageModule {}
+
--- src/app/addtask/addtask.page.ts
+++ src/app/addtask/addtask.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+//import { ImagePicker } from '@ionic-native/image-picker/ngx';
 
 @Component({
   selector: 'app-addtask',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addtask.page.scss'],
 })
 export class AddtaskPage implements OnInit {
+  validations_form: FormGroup;
+  image: any;
 
-  constructor() { }
+
+  constructor(  //private imagePicker: ImagePicker,
+    public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController,
+    public router: Router,
+    private formBuilder: FormBuilder,
+    private firebaseService: FirebaseService,
+   
+  ) { }
 
   ngOnInit() {
+    this.resetFields();
+  }
+  resetFields(): any {
+    throw new Error("Method not implemented.");
   }
 
 }
