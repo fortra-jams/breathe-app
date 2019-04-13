5601eaf86780582885ee7e27dc4bbe8637966d90
pepe moe <muhammad24lokman@gmail.com>
2019-04-14 01:07:55 +0800

    details page details resolver update task delete task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'addtask', loadChildren: './addtask/addtask.module#AddtaskPageModule' },  { path: 'alltask', loadChildren: './alltask/alltask.module#AlltaskPageModule' },
+  { path: 'addtask', loadChildren: './addtask/addtask.module#AddtaskPageModule' },
+  { path: 'alltask', loadChildren: './alltask/alltask.module#AlltaskPageModule' },
+  { path: 'details/:id', loadChildren: './details/details.module#DetailsPageModule' },
 
 ];
 
