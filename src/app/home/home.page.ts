44e6ce09f64b6e15487143cc70aca0a765acf52e
popsicle97 <saramdee97@gmail.com>
2019-04-16 13:13:23 +0800

    b

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,48 @@
-import { Component } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { MenuController,LoadingController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  items: Array<any>;
 
   userEmail: string;
 
   constructor(private menu: MenuController,
+    public loadingCtrl: LoadingController,
+
     private authService: AuthService,
-    private router: Router) { }
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(){
     if(this.authService.userDetails){
       this.userEmail = this.authService.userDetails().email;
       console.log(this.userEmail);
     }
+  }
+
+  async getData(){
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...'
+    });
+    this.presentLoading(loading);
+
+    this.route.data.subscribe(routeData => {
+      routeData['data'].subscribe(data => {
+        loading.dismiss();
+        this.items = data;
+      })
+    })
+  }
+  async presentLoading(loading) {
+    return await loading.present();
   }
 
   openEnd() {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBMxRIUAWvQxlxqyZTFtOdBCkH-C42JcYk",
+    authDomain: "breathe-3dff0.firebaseapp.com",
+    databaseURL: "https://breathe-3dff0.firebaseio.com",
+    projectId: "breathe-3dff0",
+    storageBucket: "breathe-3dff0.appspot.com",
+    messagingSenderId: "766036595309"
+  }
 };
