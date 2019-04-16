3dd3b78991a5e4e3a8f78ce0edb0230b7876b289
ashello <jordanlaiyongsheng@gmail.com>
2019-04-16 14:22:45 +0800

    barchart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -49,5 +49,16 @@ public chartClicked(e:any):void {
 
 public chartHovered(e:any):void {
   console.log(e);
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+  
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+  
+  
 }
-}
+  
